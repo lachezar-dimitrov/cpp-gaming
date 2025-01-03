485945f51690ec342911a92eff3d8c122b1a15fe
Lachezar Dimitrov <lachezar_dimitrov@outlook.com>
2025-01-03 14:07:26 +0200

    feat: add obstacle and extract collision to a function

--- sfml_test.cpp
+++ sfml_test.cpp
@@ -15,6 +15,40 @@ void logScreenModes()
     }
 }
 
+void logCollision(sf::Vector2f firstObject, sf::Vector2f secondObject)
+{
+    std::cout << "Collision Detected:"
+              << "player: X - " << firstObject.x
+              << "player: Y - " << firstObject.y
+              << "enemy: X - " << secondObject.x
+              << "enemy: Y - " << secondObject.y << std::endl;
+}
+
+bool hasCollision(sf::FloatRect mainObject, sf::FloatRect collidble)
+{
+    if (mainObject.findIntersection(collidble))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+// Implement camera movement
+// 1. If the object goes left or fight of the screen we should move the camera with it
+// 2. The ground should be large
+// 3. The ground should be infinite
+
+// Implement gravity
+// [V] 1. Make the object move on its own
+// 2. Find a way to change the text in the text object
+// 3. Display the speed of the object while moving it
+// 4. Make the object to accelerate
+// 5. Accelerate with the gravity
+
+// Implement Jump
+// [V] 1. Disable gravity while jumping
+
 int main()
 {
     logScreenModes();
@@ -24,26 +58,11 @@ int main()
 
     sf::Font font("arial.ttf");
     sf::Text text(font);
-    text.setString("Hello world");
+    text.setString("Stats: ");
     text.setCharacterSize(24); // in pixels, not points!
     text.setFillColor(sf::Color::Red);
     text.setPosition(sf::Vector2f(350.0f, 10.0f));
     // text.setStyle(sf::Text::Bold | sf::Text::Underlined);
-
-    // Implement camera movement
-    // 1. If the object goes left or fight of the screen we should move the camera with it
-    // 2. The ground should be large
-    // 3. The ground should be infinite
-
-    // Implement gravity
-    // 1. Find a way to change the text in the text object
-    // 2. Display the speed of the object while moving it
-    // 3. Make the object move on its own
-    // 4. Make the object to accelerate
-    // 5. Accelerate with the gravity
-
-    // Implement Jump
-    // 1. Disable gravity while jumping
 
     sf::CircleShape player(50);
     player.setFillColor(sf::Color::Yellow);
@@ -52,6 +71,10 @@ int main()
     sf::RectangleShape ground(sf::Vector2f(700.0f, 100.0f));
     ground.setFillColor(sf::Color::Green);
     ground.setPosition(sf::Vector2f(0.0f, 700.0f));
+
+    sf::RectangleShape obstacle(sf::Vector2f(200.0f, 100.0f));
+    obstacle.setFillColor(sf::Color::Magenta);
+    obstacle.setPosition(sf::Vector2f(600.0f, 400.0f));
 
     while (window.isOpen())
     {
@@ -63,16 +86,16 @@ int main()
             }
         }
 
+        // User input
         sf::Vector2f offset(0.0f, 0.0f);
-        sf::Vector2f originalPosition = player.getPosition();
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
         {
-            offset.x -= 0.5f;
+            offset.x -= 0.3f;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
         {
-            offset.x += 0.5f;
+            offset.x += 0.3f;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
         {
@@ -82,36 +105,35 @@ int main()
         {
             offset.y += 0.5f;
         }
+        // User input
 
         // Gravity
         offset.y += 0.1f;
+        // Gravity
+
+        // Collision detection
+        sf::Vector2f originalPosition = player.getPosition();
 
         player.move(offset);
 
-        if (ground.getGlobalBounds().findIntersection(player.getGlobalBounds()))
+        if (hasCollision(player.getGlobalBounds(), ground.getGlobalBounds()) || hasCollision(player.getGlobalBounds(), obstacle.getGlobalBounds()))
         {
-            int playerBoundsX = player.getGlobalBounds().position.x;
-            int playerBoundsY = player.getGlobalBounds().position.y;
-
-            int enemyBoundsX = ground.getGlobalBounds().position.x;
-            int enemyBoundsY = ground.getGlobalBounds().position.y;
-
-            std::cout << "Collision Detected:"
-                      << "player: X - " << playerBoundsX
-                      << "player: Y - " << playerBoundsY
-                      << "enemy: X - " << enemyBoundsX
-                      << "enemy: Y - " << enemyBoundsY << std::endl;
+            logCollision(player.getGlobalBounds().position, ground.getGlobalBounds().position);
 
             player.setPosition(originalPosition);
         }
+        // Collision detection
 
+        // Rendering
         window.clear(sf::Color::Blue);
 
         window.draw(text);
         window.draw(player);
+        window.draw(obstacle);
         window.draw(ground);
 
         window.display();
+        // Rendering
     }
 
     return 0;
